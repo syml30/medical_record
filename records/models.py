0fe07fedc3ff03272607da02e4868547f363ecc4
navid <navidsoleymani@ymail.com>
2022-02-21 15:22:11 +0330

    create models.py

--- records/models.py
+++ records/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 
+class RecordModel(models.Model):
+    user = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False,
+    )
+    data = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False,
+    )
