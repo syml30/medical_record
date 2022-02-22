3c4d926d072e3cde5c6aa895785dc5f91b45aef3
yasaman <y.mirkeshvari@gmail.com>
2022-02-22 12:40:11 +0330

    CU-1zc1ch4 :: create model for db

--- records/models.py
+++ records/models.py
@@ -15,3 +15,6 @@ class RecordModel(models.Model):
     create_date_time = models.DateTimeField(
         auto_now_add=True,
     )
+
+    def __str__(self):
+        return f"{self.user}-{self.data}"
