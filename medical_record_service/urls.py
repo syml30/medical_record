687378f43844f601b285db76e2e815ac299966b2
yasaman <y.mirkeshvari@gmail.com>
2022-02-22 12:42:11 +0330

    CU-1zc1ch4 :: add root url and swagger

--- medical_record_service/urls.py
+++ medical_record_service/urls.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
-from django.urls import path
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+from django.urls import path, include, re_path
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Medical Record Service API",
+        default_version='v0.01',
+        description="This service is used to medical record",
+        contact=openapi.Contact(email="zidevgroup@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+                  path('admin/', admin.site.urls),
+                  path('api/records/', include('records.urls')),
+                  re_path(r'^api/doc(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0),
+                          name='documentation_json'),
+                  re_path(r'^api/doc/$', schema_view.with_ui('swagger', cache_timeout=0), name='documentation'),
+                  re_path(r'^api/redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema_redoc'),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+              + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
