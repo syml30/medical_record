ea1c584c3d87aa57672c7b328313185b5dbff01a
yasaman <y.mirkeshvari@gmail.com>
2022-02-22 12:44:30 +0330

    CU-1zc1ch4 :: add list,create,retrieve api view

--- records/views.py
+++ records/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
 
-# Create your views here.
+from records.models import RecordModel
+from records.serializers import RecordModelSerializer
+
+
+class ListRecordModel(ListAPIView):
+    serializer_class = RecordModelSerializer
+    queryset = RecordModel.objects.all()
+
+
+class RetrieveRecordModel(RetrieveAPIView):
+
+    lookup_field = 'pk'
+    serializer_class = RecordModelSerializer
+    queryset = RecordModel.objects.all()
+
+
+class CreateRecordModel(CreateAPIView):
+    serializer_class = RecordModelSerializer
+    queryset = RecordModel.objects.all()
